24122b78ddd8eba309da3278ee90eed4af3aff12
Crosve Lucero <crosve.lucero73@myhunter.cuny.edu>
2024-03-16 14:58:52 -0400

    method errors on production

--- pages/api/userinjury.jsx
+++ pages/api/userinjury.jsx
@@ -2,19 +2,15 @@ import admin from "@/app/lib/firebase/firebase";
 import { db } from "@/app/lib/firebase/firebase";
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const { uid } = req.body;
-    const userDocRef = db.collection("users").doc(uid);
-    const userDoc = await userDocRef.get();
+  const { uid } = req.body;
+  const userDocRef = db.collection("users").doc(uid);
+  const userDoc = await userDocRef.get();
 
-    if (!userDoc.exists) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    const userData = userDoc.data();
-    const injuryPlan = userData.injuryPlan;
-    return res.status(200).json(injuryPlan);
-  } else {
-    return res.status(405).json({ message: "Method not allowed" });
+  if (!userDoc.exists) {
+    return res.status(404).json({ message: "User not found" });
   }
+
+  const userData = userDoc.data();
+  const injuryPlan = userData.injuryPlan;
+  return res.status(200).json(injuryPlan);
 }
