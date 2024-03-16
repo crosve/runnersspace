980ca8c5a78f8b37791dd30c5c08bf7dae34c558
Crosve Lucero <crosve.lucero73@myhunter.cuny.edu>
2024-03-16 13:55:29 -0400

    user injury plan

--- app/lib/firebase/fireconfig.js
+++ app/lib/firebase/fireconfig.js
@@ -1,17 +1,3 @@
-// export const fireConfig= {
-//   "type": "service_account",
-//   "project_id": "runners-b1968",
-//   "private_key_id": "2e2bd081873006eba1fa19b31bcc6d19275653f7",
-//   "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDNE1luR7zUmzzl\nK92eOldiW0c3U6Adid6nDiZ17hNcHnfLJnX704Zh0+wIYYPcLZlt23gCj2egsKXC\nNNdgb76DyfXcKmLoBLurjgD4cnM7HUcTnDPb/V1uoP7v211pDNs16BG1gfyuxmU5\n5cfVgNaV5tnmTuptdhFyu/yRWFuobDtDAySqJHDevzHIvizMwAOxxQqltTPFGcFF\nTe0/0LsgKbu6R2iOYyDQ1umGHerRxrR3v/HYU3yZ5xnP5VYlEKwk1Np1Rv18iL58\n1dCfDBN+C3GhmcFgtrS07SYfNc85g2LhCMpOwIg3PhUCBzbyUJG0HnmK4NFnMhkL\nCa6zfT61AgMBAAECggEASwceO/ug+UPGVq3iyeXcXI66kmJU/RmYzROfPJwalyRK\nfZeXqMdtrdLj0vkiTeac8JVP5dfjeOXH7zJ37b9UO1EjUyZqwyogIxa0AjGVOPZw\nL5uBWaXrjfL28ngWR729YY6DFB33fVX2aOVJZDJbz8ICzIDowZ1lqs1eDSr/dbFj\nCIUGPlFBWlMz7MX6kkeEn/DTeGxH/ddkMKmJ8O8MPaLcrtyxdijC2E7Icvio2YW0\nXYpHcY6AFA/W36wUj+PDrMAjVx6nZlZZPmSr/OFZV4hGRBiKMUaXQLuWATpRnTgD\nMNx8zHNwA2Z9mLtbcxFtvYws0u+UEhS3g67v9JwrrQKBgQD5Q2x5/Y39qagLdBBP\nCxuBuJ8pe8aoCyTbBmuv/QfVNMhHM2fH3of/tr7hSV/sLx90iwEtJXXBl3pvTbhW\nyc7uYT0MPAQpUNfGpTJkOGEgnJhqZq/mTe0VGuSqV+4V5p4ZAg8hLSvwIXt1oKj3\nSB1rG0auPy6abKh93V2+v2RC5wKBgQDSnjQ09Syy6QWzdVHI9H7nmgtEsPGgg1Rc\neGns8bcWUjrrOZGbrlkgmXDztU32wP272VPkjLUelj88jWXOiAWEfNbUkCDcKvJT\nbnjJWSWwIUTtc7+rEnAEa5GpwVGqcfVwNozzvfOn/syuJOUK597xsXm3S/K2vfhB\nU/m7qbMaAwKBgQD2poMmy4G/qivA+Z5RuPoFCP1l/Tb8fdqAaQASa11e2hX8bp+Z\nJViJ7vk+0G58tbx4hDwrwk+duxjKHqv0JUoAlHPmMVUk1PxPcpREm41wycQANZCF\nYkLhiXUQImZTgZqbwG/cMmix+0/Be7L2zoNdwuXT8m1ZHYA02+GxqI27rQKBgDTX\n4qti1EszMe37woBT7782JBug9Y7bRIBK6fC8GL6t9QBtmj3gOZZ3SSKsW5pMg5zt\nAyJ1tz+sXMNZirCN6ix6kgAVBVmYIOKT9hQ2nMEbJ8sX3oASiS5iutJHobDNFNKJ\nrUW+Y/LDeCE9OoZI9OPZLnuxv3zgHvhtFFxkUH53AoGAchwODOhuTk61bukOqtMw\nFfBGli1obz/9hXAVzi8xejBUOhPF9flVG+j639G4Ax+RJWcOIk9Ngcmh5QCDO2OB\neiDDUJzGCCnDgmSmN78z9qHnUPnTN2lNs6if4ViG9m5NEe+RmwvyrubPST95uiJ+\nREwisBEDInRfUjujPZNVhec=\n-----END PRIVATE KEY-----\n",
-//   "client_email": "firebase-adminsdk-yfabk@runners-b1968.iam.gserviceaccount.com",
-//   "client_id": "105047884631960926336",
-//   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-//   "token_uri": "https://oauth2.googleapis.com/token",
-//   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-//   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-yfabk%40runners-b1968.iam.gserviceaccount.com",
-//   "universe_domain": "googleapis.com"
-// }
-
 
 export const fireConfig = {
   type: process.env.NEXT_PUBLIC_FIREBASE_TYPE,
