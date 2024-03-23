b2ed0b635b9f10b8157a323679eba9b01a77e270
Crosve Lucero <crosve.lucero73@myhunter.cuny.edu>
2024-03-23 09:26:43 -0400

    fixed form error

--- app/dashboard/gear/page.jsx
+++ app/dashboard/gear/page.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Form from "../../components/gearPage/form";
+// import Form from "../../components/gearPage/form";
 import Items from "../../components/gearPage/Items";
 
 function Gear() {
   return (
     <div className="flex flex-col justify-center bg-gray-100 text-4xl font-bold pt-24 overflow-auto">
-      <div>
+      {/* <div>
         <Form />
-      </div>
+      </div> */}
 
       <div className="relative w-full p-6">
         <Items />
